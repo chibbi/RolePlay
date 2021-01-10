a3fdb236fd05e17168d1070328ecebfc2e91cb4a
chibbi <chehab.djibril@gmail.com>
2021-01-11 00:32:56 +0100

    Successful Build, added Kit

--- rolePlay/src/main/java/org/ranin/rolePlay/App.java
+++ rolePlay/src/main/java/org/ranin/rolePlay/App.java
@@ -4,7 +4,7 @@ public class App extends JavaPlugin {
     @Override
     public void onEnable() {
         // Register our command "kit" (set an instance of your command class as executor)
-        this.getCommand("kit").setExecutor(new CommandKit());
+        this.getCommand("kit").setExecutor(new Commands());
         getLogger().info("Hello, SpigotMC!");
     }
     @Override
