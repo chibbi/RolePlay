57cfbc2f157f3c4370abbdb015fa89bad247480c
chibbi <chehab.djibril@gmail.com>
2021-01-11 00:16:13 +0100

    added Command

--- rolePlay/src/main/java/org/ranin/rolePlay/App.java
+++ rolePlay/src/main/java/org/ranin/rolePlay/App.java
@@ -3,6 +3,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class App extends JavaPlugin {
     @Override
     public void onEnable() {
+        // Register our command "kit" (set an instance of your command class as executor)
+        this.getCommand("kit").setExecutor(new CommandKit());
         getLogger().info("Hello, SpigotMC!");
     }
     @Override
