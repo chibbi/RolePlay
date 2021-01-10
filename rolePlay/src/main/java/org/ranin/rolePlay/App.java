5287c2cf1c318f83871d3c48fceb6d0c5b8dcaf6
chibbi <chehab.djibril@gmail.com>
2021-01-11 00:14:22 +0100

    Compiled

--- rolePlay/src/main/java/org/ranin/rolePlay/App.java
+++ rolePlay/src/main/java/org/ranin/rolePlay/App.java
@@ -1,13 +1,12 @@
 package org.ranin.rolePlay;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.bukkit.plugin.java.JavaPlugin;
+public class App extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello, SpigotMC!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("See you again, SpigotMC!");
     }
 }
